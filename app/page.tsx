4aa297649e9e912b3d60da19f93dbd46186f0e13
IT-Security-Support <it-security@ITSecurySupport.speedport.ip>
2026-05-21 23:03:03 +0200

    fix logout spacing

--- app/page.tsx
+++ app/page.tsx
@@ -423,7 +423,7 @@ export default function Home() {
 
           <button
             onClick={logout}
-            className="bg-red-600 px-4 py-2 rounded-xl mt-8"
+            className="bg-red-600 px-4 py-2 rounded-xl mt-11"
           >
             Logout
           </button>
